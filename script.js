0467a2141472ca0c3e2322bac88201cca05f0536
VeryGoodDev <very_good_dev@outlook.com>
2021-03-16 22:22:38 -0600

    Functionality basically done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector(`.input`)
 const output = document.querySelector(`.output`)
 const parseBtn = document.querySelector(`.parse-btn`)
 const siteInfoEl = output.querySelector(`.site-info`)
-const userAgentsTable = output.querySelector(`.user-agents`)
+const userAgentsTable = output.querySelector(`.user-agents tbody`)
 
 parseBtn.addEventListener(`click`, handleParseClick)
 
@@ -13,6 +13,7 @@ function handleParseClick() {
   const parsed = parseInput(input.value)
   updateSiteInfo(parsed.siteInfo)
   updateUserAgents(parsed.userAgents)
+  console.log(parsed.userAgents)
 }
 function parseInput(rawText) {
   const lines = rawText.split(`\n`).filter((line) => line.length && !line.startsWith(`#`))
@@ -22,8 +23,9 @@ function parseInput(rawText) {
   const userAgents = {}
   let currentUserAgent
   lines.forEach((line) => {
-    const [directiveRaw, value] = line.split(/:\s*/)
+    const [directiveRaw, ...valueRaw] = line.split(/:\s*/)
     const directive = directiveRaw.toLowerCase()
+    const value = valueRaw.join(`:`)
     if (directive === `user-agent`) {
       currentUserAgent = value
       if (!userAgents[currentUserAgent]) {
@@ -34,8 +36,10 @@ function parseInput(rawText) {
         userAgents[currentUserAgent].allow.push(value)
       } else if (directive === `disallow`) {
         userAgents[currentUserAgent].disallow.push(value)
+      } else if (directive === `crawl-delay`) {
+        userAgents[currentUserAgent].crawlDelay = value
       } else {
-        userAgents[currentUserAgent][directiveRaw] = value
+        userAgents[currentUserAgent][directive] = value
       }
     } else if (directive === `sitemap`) {
       siteInfo.sitemaps.push(value)
@@ -49,6 +53,7 @@ function updateSiteInfo(siteInfo) {
   if (isEmpty(siteInfo)) return
   siteInfoEl.innerHTML = ``
   Object.entries(siteInfo).forEach(([key, value]) => {
+    if (!value || (Array.isArray(value) && !value.length)) return
     const item = document.createElement(`div`)
     item.classList.add(`site-info-item`)
     const strong = document.createElement(`strong`)
@@ -66,7 +71,42 @@ function updateSiteInfo(siteInfo) {
     siteInfoEl.appendChild(item)
   })
 }
-function updateUserAgents(userAgents) {}
+function updateUserAgents(userAgents) {
+  Object.entries(userAgents).forEach(([userAgent, { allow, disallow, crawlDelay }]) => {
+    const row = document.createElement(`tr`)
+    const userAgentTd = document.createElement(`td`)
+    userAgentTd.textContent = userAgent
+    const allowTd = document.createElement(`td`)
+    if (allow.length) {
+      allowTd.innerHTML = allow
+        .map((url) => {
+          const span = document.createElement(`span`)
+          span.classList.add(`url`)
+          span.textContent = url
+          return span.outerHTML
+        })
+        .join(``)
+    }
+    const disallowTd = document.createElement(`td`)
+    if (disallow.length) {
+      disallowTd.innerHTML = disallow
+        .map((url) => {
+          const span = document.createElement(`span`)
+          span.classList.add(`url`)
+          span.textContent = url
+          return span.outerHTML
+        })
+        .join(``)
+    }
+    const crawDelayTd = document.createElement(`td`)
+    crawDelayTd.textContent = crawlDelay ?? ``
+    row.appendChild(userAgentTd)
+    row.appendChild(allowTd)
+    row.appendChild(disallowTd)
+    row.appendChild(crawDelayTd)
+    userAgentsTable.appendChild(row)
+  })
+}
 
 // Helper functions
 function getUserAgentEntry() {
